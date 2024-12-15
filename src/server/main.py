b014ed555be1d7333c257fc722b3a379ca1d7d89
SYNER-CATALYST <syner.catalyst@gmail.com>
2024-12-15 05:36:35 +0000

    Set Up Server --> SSH Connection

--- src/server/main.py
+++ src/server/main.py
@@ -1,15 +1,24 @@
 import typer
 import os
+import sys
+import ansible_runner
 
 from typing_extensions import Annotated
 
 app = typer.Typer(no_args_is_help=True)
 
 @app.command(no_args_is_help=True)
-def ssh_setup(ssh_login: Annotated[str, typer.Argument()], ssh_port: Annotated[int, typer.Argument()] = 22):
+def ssh_setup(ssh_host: Annotated[str, typer.Argument(help="For Specific Host(s) or All")]):
     print("Setting Up SSH Key to Remote Host...")
-    os.system(f'ssh-copy-id -p {ssh_port} {ssh_login}')
+    os.chdir('/opt/cstation/ansible_playbook/server/')
+    print(f'Setting Up SSH Connection for Host(s): {ssh_host}')
+    if ssh_host == "All":
+        os.system('ansible-playbook server_ssh.yaml')
+    else:
+        os.system(f'ansible-playbook -l {ssh_host} server_ssh.yaml')
 
 
 if __name__ == "__main__":
     app()
+
+
