322f020a04eec3848e5ef39cd2cf8aee0f12b8cc
Loh W S, Wilson <loh.wilson@gmail.com>
2025-02-02 20:32:28 +0800

    [UPD] Odoo Rsync Module

--- src/github/repo.py
+++ src/github/repo.py
@@ -1,5 +1,6 @@
 import typer
-import os
+import yaml
+import ansible_runner
 
 from pathlib import Path
 from typing import Optional
@@ -7,19 +8,38 @@ from typing_extensions import Annotated
 
 app = typer.Typer()
 
+
+def load_options(option_file: Path):
+    """
+        Load options from a YAML file.
+    """
+    if option_file.exists():
+        with open(option_file, "r") as file:
+            print(f"Loading options from {option_file}")
+            return yaml.safe_load(file)
+    
+
 @app.command()
-def sync(
-    config: Annotated[
-        typer.FileText, typer.Option()
-    ] = "/opt/cstation/etc/github_repo.yaml",
+def odoo_sync(
+    command_file: Path = typer.Option(
+        Path("/opt/cstation/ansible_playbook/github/repo_odoo_sync.yaml"),
+        help="Sync Odoo Repositories from Github",
+        file_okay=True,  # Allow files
+        dir_okay=False,  # Disallow directories
+        readable=True,  # Ensure the file is readable
+    ),
 ):
     """
-        Sync the Github repositories with the upstream repositories
+    Sync the Lcal Odoo repositories with the Github ANSIS repositories
+    Using /opt/cstation/ansible_playbook/github/repo_odoo_sync.yaml Playbook
     """
-    for line in config:
-        if line.strip() != "" or line.startswith('#'):
-            print(f"gh repo sync {line.strip()}")
-            os.system(f"gh repo sync {line.strip()}")
+    result = ansible_runner.run(
+        playbook="/opt/cstation/ansible_playbook/github/repo_odoo_sync.yaml",
+    )
+    print(result)
+
 
 if __name__ == "__main__":
     app()
+
+
