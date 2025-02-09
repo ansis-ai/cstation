f2edd22656e6ebb9728259e5e0bfe57fbdca0fee
Loh W S, Wilson <loh.wilson@gmail.com>
2025-02-09 17:37:36 +0800

    [ADD] odoo command

--- src/cstation.py
+++ src/cstation.py
@@ -4,13 +4,15 @@ import typer
 from docker import main as docker
 from github import main as github
 from local import main as local
+from odoo import main as odoo
 from server import main as server
 
 app = typer.Typer(no_args_is_help=True, help='Control Station (Internal Use)', rich_markup_mode="markdown")
 
 app.add_typer(docker.app, name="docker")
 app.add_typer(github.app, name="github")
-app.add_typer(docker.app, name="local")
+app.add_typer(local.app, name="local")
+app.add_typer(odoo.app, name="odoo")
 app.add_typer(server.app, name="server")
 
 
