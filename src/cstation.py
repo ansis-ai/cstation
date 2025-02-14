81fb922eb581c867a59e8b9df175312b1def1d42
Loh W S, Wilson <loh.wilson@gmail.com>
2025-02-14 21:08:44 +0800

    [ADD] command container --> update

--- src/cstation.py
+++ src/cstation.py
@@ -1,20 +1,19 @@
 import typer
 
 
-from docker import main as docker
 from github import main as github
 from local import main as local
 from odoo import main as odoo
 from server import main as server
+from container import main as container
 
 app = typer.Typer(no_args_is_help=True, help='Control Station (Internal Use)', rich_markup_mode="markdown")
 
-app.add_typer(docker.app, name="docker")
 app.add_typer(github.app, name="github")
 app.add_typer(local.app, name="local")
 app.add_typer(odoo.app, name="odoo")
 app.add_typer(server.app, name="server")
-
+app.add_typer(container.app, name="container")
 
 if __name__ == "__main__":
     app()
--- src/server/main.py
+++ src/server/main.py
@@ -1,16 +1,19 @@
-import typer
 import os
-import sys
-import ansible_runner
+
+import typer
 from typing_extensions import Annotated
+
 from utils import get_inventory_hosts
 
-app = typer.Typer(no_args_is_help=True)
+app = typer.Typer(no_args_is_help=True, help="Server management commands")
+container_app = typer.Typer(help="Container management commands")
+app.add_typer(container_app, name="container")
+
 
 @app.command()
 def list():
     """
-        List server from inventory
+    List server from inventory
     """
     hosts_data = get_inventory_hosts()
     if not hosts_data:
@@ -27,27 +30,22 @@ def list():
             print(f"{host}: {display_host}  ({user}@{port})")
 
 
-
 @app.command(no_args_is_help=True)
 def ssh_setup(
-    server: Annotated[
-        str, typer.Argument(help="server name or group name (All)")
-    ],
+    server: Annotated[str, typer.Argument(help="server name or group name (All)")],
 ):
     """
-        Setting up SSH key for server(s) \n
-        For server name: cstation server list
+    Setting up SSH key for server(s) \n
+    For server name: cstation server list
     """
     print("Setting up SSH key to remote server ...")
-    os.chdir('/opt/cstation/ansible_playbook/server/')
-    print(f'Setting up SSH connection for host(s): {server}')
-    if ssh_host == "All":
-        os.system('ansible-playbook server_ssh.yml')
+    os.chdir("/opt/cstation/ansible_playbook/server/")
+    print(f"Setting up SSH connection for host(s): {server}")
+    if server == "All":
+        os.system("ansible-playbook server_ssh.yml")
     else:
-        os.system(f'ansible-playbook -l {server} server_ssh.yml')
+        os.system(f"ansible-playbook -l {server} server_ssh.yml")
 
 
 if __name__ == "__main__":
     app()
-
-
